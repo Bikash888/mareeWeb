623a9de354de50c8444888b694312a166c8d6903
Bikash888 <bikashdulal150@gmail.com>
2020-05-11 17:18:42 +0545

    Updates

---
+++ precache-manifest.2249de06dd5defdb12abf47cdf5353de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b351d0f258cff1e58f700a22529849",
+    "url": "/Maree/index.html"
+  },
+  {
+    "revision": "c910a6f192284c68a578",
+    "url": "/Maree/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "964959b00ce3280f9480",
+    "url": "/Maree/static/css/main.dbe64193.chunk.css"
+  },
+  {
+    "revision": "c910a6f192284c68a578",
+    "url": "/Maree/static/js/2.d716c00a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Maree/static/js/2.d716c00a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "964959b00ce3280f9480",
+    "url": "/Maree/static/js/main.4c363caf.chunk.js"
+  },
+  {
+    "revision": "ec5c1cbab5355b691b57",
+    "url": "/Maree/static/js/runtime-main.af1679fe.js"
+  },
+  {
+    "revision": "a6749496ee90671b65c6bb96c3a53cb8",
+    "url": "/Maree/static/media/hamburger.a6749496.svg"
+  }
+]);
