b105893730d7bd64ec053448dad83d66a94eaf7c
Bikash888 <bikashdulal150@gmail.com>
2020-05-11 17:41:38 +0545

    complete

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { unregister } from './registerServiceWorker';
 
-unregister();
 
 ReactDOM.render(
   <React.StrictMode>
